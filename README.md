d728249806966c68f4bed73b9a91de28c4dc7b18
Hank Wang <hankwang.whc@gmail.com>
2018-03-11 15:04:18 +0800

    改善了传参过程。前端控件有问题、safari读取不了、chrome需要 右键-显示控件

--- README.md
+++ README.md
@@ -16,3 +16,18 @@
 - 登陆 登出
 - 详细信息 填写/查看
 
+# 播放器界面的实现
+
+## 渲染界面（play）
+
+- 暂时属于静态html，通过调用AJAX对象来向后端要数据
+
+## AJAX播放器（play_ajax_obj）
+
+- 是一个html的对象、生成一个播放器
+- 传入一个name参数（在jquery中），生成一个src视频源url
+
+## 文件对象（play_file_obj）
+
+- 传入一个src参数（通过AJAX传入），通过src找文件
+- 是一个FileResponse
--- curriculum/play.py
+++ curriculum/play.py
@@ -6,19 +6,24 @@ import os
 PLAY_PATH = '/Users/wanghc/Desktop/play_list'
 
 
+# 渲染播放界面，播放器靠ajax实现
 def play_view(request):
     return render(request, 'curriculum/play.html')
 
 
-# AJAX对象，用来返回一个播放器对象，具体的视频需要继续调用其他url
+# AJAX对象，用来返回一个播放器对象，具体的视频文件需要继续调用其他url
+# 参数：name：用来生成一个src（视频源url），传给play_file_obj（src）
 def play_ajax_obj(request, name):
-    src = os.path.join("play_file_obj/", name)
+    # src = os.path.join("play_file_obj/", name)
     # name = name + '.mp4'
     # file_path = os.path.join(PLAY_PATH, name)
     # response = FileResponse(open(file, 'rb'), content_type='video/mp4')
-    return render(request, 'curriculum/play_ajax_obj.html', {'src': src})
+    return render(request, 'curriculum/play_ajax_obj.html', {
+        'name': name,
+    })
 
 
+# 用来返回一个文件对象，用来填充视频内容
 def play_file_obj(request, src):
     src = os.path.join(PLAY_PATH, src)+'.mp4'
     response = FileResponse(open(src, 'rb'), content_type='video/mp4')
